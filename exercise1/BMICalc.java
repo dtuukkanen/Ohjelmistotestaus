f5dbe44c0cab81a0e23c2b6fcb2629314a8faa6a
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2023-09-07 11:25:30 +0300

    Exercise 1 is done

---
+++ exercise1/BMICalc.java
@@ -0,0 +1,27 @@
+
+public class BMICalc {
+
+    public static void main(String[] args) {
+
+    }
+
+    public double calculateBMI(double height, double weight) {
+        if (height == 0 || weight == 0) {
+            return -1;
+        }
+        return Math.round(weight / (height * height) * 10) / 10.0;
+    }
+
+    public String getBMICategoryString(double height, double weight) {
+        double BMI = calculateBMI(height, weight);
+        if (BMI < 18.5) {
+            return "Underweight";
+        } else if (BMI < 25) {
+            return "Normal weight";
+        } else if (BMI < 30) {
+            return "Overweight";
+        } else {
+            return "Obese";
+        }
+    }
+}
