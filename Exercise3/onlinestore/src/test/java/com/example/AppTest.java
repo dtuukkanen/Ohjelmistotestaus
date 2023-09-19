5cfcf35d1302bac0155219a5533d083e4898a929
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2023-09-19 22:52:05 +0300

    Unused import removed

--- Exercise3/onlinestore/src/test/java/com/example/AppTest.java
+++ Exercise3/onlinestore/src/test/java/com/example/AppTest.java
@@ -2,7 +2,6 @@ package com.example;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
